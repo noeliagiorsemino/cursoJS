be26949048f1cbf7d255b5115d4d81b7cac9bf10
noeliagiorsemino <54905617+noeliagiorsemino@users.noreply.github.com>
2019-09-11 16:44:52 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,7 +1,31 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
+
+switch(mes){
+    case "Enero":
+    alert("verano");
+    break;
+    case "Marzo":
+    alert("A cleases");
+    break;
+    case "Julio":
+    alert("se vienen las vacaciones");
+    break;
+    case "Diciembre":
+    alert("Felices Fiestas");
+    break;
+    default:
+    alert("Default");
+    
+}
+
+/*para no repetir alert
+        case "Enero":
+        case "Marzo":
+        alert("verano");
+        Break*/
 
 
 
--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
-	alert('iteración while');
+	//alert('iteración while');
+
+	var contador = 0;
+
+	while( contador < 10){
+
+		console.log("Cont:" + contador);
+
+		contador++;
+	}
+
+	/*if(contador == 4){
+		break;
+	} es para salir de la iteracion cuando lo desee*/
+	/*cotinue vuelve a empezar mientras se compla la condicion, pero puedo generar un bucle infinito*/
 
 
 }//FIN DE LA FUNCIÓN
