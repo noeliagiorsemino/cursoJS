4aba16ab1f57e64b2973af934b27a0418cc3b3f9
alumno <alumno@utnfra.com.ar>
2019-10-09 16:49:25 -0300

    .

--- CLASESPAMI/Ej6/index.js
+++ CLASESPAMI/Ej6/index.js
@@ -8,16 +8,15 @@ let primerHijo = div.children;
 
 //hijos[1].style.backgroundColor = "red";
 
-let p1 = document.getElementById('p1');
+let p1 = document.getElementById("p1");
 //console.log(p1);
 let padre = p1.parentNode;
 
 console.log(padre);
 
-
 //let titulo = document.getElementById('titulo'); es lo mismo que esta a continuacion
 
-let titulo = document.getElementsByTagName('h1')[0];
+let titulo = document.getElementsByTagName("h1")[0];
 
 //console.log(titulo);
 
